38d5466aebb345b7c9a111c5a64664e7c80bf00c
yurygavrilov47 <yurygavrilov47@gmail.com>
2019-12-26 20:52:45 +0300

    aaa

---
+++ cmake-build-debug/CMakeFiles/shared_ptr.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shared_ptr.dir/sources/main.cpp.o"
+  "libshared_ptr.dll.a"
+  "shared_ptr.exe"
+  "shared_ptr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shared_ptr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
